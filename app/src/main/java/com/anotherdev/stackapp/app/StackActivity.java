cc6455fba4b438d52a8a931da3e6fa54df7a6116
Polson <>
2015-07-07 16:02:56 +0700

    - bind layout and init toolbar if exist

--- app/src/main/java/com/anotherdev/stackapp/app/StackActivity.java
+++ app/src/main/java/com/anotherdev/stackapp/app/StackActivity.java
@@ -2,16 +2,42 @@ package com.anotherdev.stackapp.app;
 
 import android.os.Bundle;
 import android.support.annotation.LayoutRes;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+
+import com.anotherdev.stackapp.R;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
 public abstract class StackActivity extends AppCompatActivity {
+
+    @Nullable @Bind(R.id.toolbar) Toolbar mToolbar;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        final int activityLayout = getActivityLayout();
+        if (activityLayout > 0) {
+            setContentView(activityLayout);
+            ButterKnife.bind(this);
 
+            if (mToolbar != null) {
+                setSupportActionBar(mToolbar);
+                final ActionBar actionBar = getSupportActionBar();
+                if (actionBar != null) {
+                    setupSupportActionBar(actionBar);
+                }
+            }
+        }
     }
 
     @LayoutRes
     protected abstract int getActivityLayout();
+
+    protected void setupSupportActionBar(@NonNull ActionBar actionBar) {}
 }
