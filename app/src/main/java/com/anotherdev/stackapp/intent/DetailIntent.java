b227461026fd6d5090e6ef4f22c631474c2a2d3e
Polson <>
2015-07-08 21:10:00 +0700

    add question payload

--- app/src/main/java/com/anotherdev/stackapp/intent/DetailIntent.java
+++ app/src/main/java/com/anotherdev/stackapp/intent/DetailIntent.java
@@ -1,12 +1,29 @@
 package com.anotherdev.stackapp.intent;
 
 import android.content.Context;
+import android.content.Intent;
+import android.os.Parcelable;
 
+import com.anotherdev.stackapp.api.stackexchange.Question;
 import com.anotherdev.stackapp.app.DetailActivity;
+
+import org.parceler.Parcels;
 
 public class DetailIntent extends StackIntent {
 
-    public DetailIntent(Context context) {
+    private static final String EXTRA_QUESTION = EXTRA + "question";
+
+
+    public DetailIntent(Context context, Question question) {
         super(context, DetailActivity.class);
+        if (question != null) {
+            putExtra(EXTRA_QUESTION, Parcels.wrap(question));
+        }
+    }
+
+
+    public static Question getQuestion(Intent intent) {
+        Parcelable parcel = intent.getParcelableExtra(EXTRA_QUESTION);
+        return Parcels.unwrap(parcel);
     }
 }
