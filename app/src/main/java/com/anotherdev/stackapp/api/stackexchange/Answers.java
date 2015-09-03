873ad0487a3403017b10ce4f1678af9d9315e11d
Polson <polson.k@gmail.com>
2015-09-03 12:08:07 +0700

    add default value

--- app/src/main/java/com/anotherdev/stackapp/api/stackexchange/Answers.java
+++ app/src/main/java/com/anotherdev/stackapp/api/stackexchange/Answers.java
@@ -2,11 +2,12 @@ package com.anotherdev.stackapp.api.stackexchange;
 
 import com.google.gson.annotations.SerializedName;
 
+import java.util.Collections;
 import java.util.List;
 
 public class Answers {
 
-    @SerializedName("items") List<Answer> answers;
+    @SerializedName("items") List<Answer> answers = Collections.emptyList();
 
 
     public List<Answer> get() {
--- app/src/main/java/com/anotherdev/stackapp/api/stackexchange/Questions.java
+++ app/src/main/java/com/anotherdev/stackapp/api/stackexchange/Questions.java
@@ -2,11 +2,12 @@ package com.anotherdev.stackapp.api.stackexchange;
 
 import com.google.gson.annotations.SerializedName;
 
+import java.util.Collections;
 import java.util.List;
 
 public class Questions {
 
-    @SerializedName("items") List<Question> questions;
+    @SerializedName("items") List<Question> questions = Collections.emptyList();
 
 
     public List<Question> get() {
