d673ed7b67e4f1d560b133a990504c32f0da4ab1
Polson <polson.k@gmail.com>
2015-09-03 18:53:38 +0700

    add caching data using realm

---
+++ app/src/main/java/com/anotherdev/stackapp/rx/RxRealmCache.java
@@ -0,0 +1,49 @@
+package com.anotherdev.stackapp.rx;
+
+import android.content.Context;
+
+import io.realm.Realm;
+import io.realm.RealmObject;
+import rx.Observable;
+import rx.Subscriber;
+import rx.functions.Func1;
+
+public class RxRealmCache {
+
+    private RxRealmCache() {
+        throw new AssertionError("No instances");
+    }
+
+    public static <T extends RealmObject> Observable.Transformer<T,T> cache(Context context, final Class<T> clazz) {
+        final Context appContext = context.getApplicationContext();
+        return new Observable.Transformer<T,T>() {
+            @Override
+            public Observable<T> call(Observable<T> source) {
+                return Observable
+                        .create(new Observable.OnSubscribe<T>() {
+                            @Override
+                            public void call(Subscriber<? super T> subscriber) {
+                                Realm realm = Realm.getInstance(appContext);
+                                T cache = realm.where(clazz).findFirst();
+                                if (cache != null) {
+                                    subscriber.onNext(cache);
+                                }
+                                subscriber.onCompleted();
+                            }
+                        })
+                        .concatWith(source)
+                        .map(new Func1<T, T>() {
+                            @Override
+                            public T call(T t) {
+                                Realm realm = Realm.getInstance(appContext);
+                                realm.beginTransaction();
+                                T managed = realm.copyToRealmOrUpdate(t);
+                                realm.commitTransaction();
+                                realm.close();
+                                return managed;
+                            }
+                        });
+            }
+        };
+    }
+}
